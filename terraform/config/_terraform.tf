1a4e53db6816d6a8ee5ce6b145c1b48f1b21d888
Ulli Goschler <770135+nce@users.noreply.github.com>
2022-10-24 20:01:50 +0200

    feat(tf): add remote tf state (#40)

--- terraform/config/_terraform.tf
+++ terraform/config/_terraform.tf
@@ -1,4 +1,26 @@
 terraform {
+  #backend "http" {
+
+  #  address        = "https://api.tfstate.dev/github/v1"
+  #  lock_address   = "https://api.tfstate.dev/github/v1/lock"
+  #  unlock_address = "https://api.tfstate.dev/github/v1/lock"
+  #  lock_method    = "PUT"
+  #  unlock_method  = "DELETE"
+  #  username       = "nce/oci-free-cloud-k8s"
+  #}
+
+  backend "s3" {
+    bucket                      = "terraform-states"
+    key                         = "config/terraform.tfstate"
+    endpoint                    = "https://frrwy4uskhkj.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
+    region                      = "eu-frankfurt-1"
+    shared_credentials_file     = "~/.oci/config"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+
   required_providers {
     oci = {
       source  = "oracle/oci"
--- terraform/infra/_terraform.tf
+++ terraform/infra/_terraform.tf
@@ -1,4 +1,17 @@
 terraform {
+
+  backend "s3" {
+    bucket                      = "terraform-states"
+    key                         = "infra/terraform.tfstate"
+    endpoint                    = "https://frrwy4uskhkj.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
+    region                      = "eu-frankfurt-1"
+    shared_credentials_file     = "~/.oci/config"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+
   required_providers {
     oci = {
       source  = "oracle/oci"
