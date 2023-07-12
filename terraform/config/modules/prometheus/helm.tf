1ff6b7fe41c4125c560381de8ba5379baf589c23
Ulli Goschler <git@nce.wtf>
2023-07-12 15:05:37 +0200

    add remote_write receiver on prometheus

--- terraform/config/modules/prometheus/helm.tf
+++ terraform/config/modules/prometheus/helm.tf
@@ -11,6 +11,25 @@ resource "helm_release" "prometheus" {
   timeout          = 120
 
   values = [<<YAML
+prometheus:
+  ingress:
+    enabled: true
+    hosts: 
+      - prometheus.klangregen.de
+    tls:
+    - secretName: prometheus-cert
+      hosts:
+        - prometheus.klangregen.de
+    annotations:
+      cert-manager.io/cluster-issuer: letsencrypt
+      acme.cert-manager.io/http01-edit-in-place: "true"
+      kubernetes.io/ingress.class: nginx
+      external-dns.alpha.kubernetes.io/hostname: prometheus.klangregen.de
+      nginx.ingress.kubernetes.io/auth-type: basic
+      nginx.ingress.kubernetes.io/auth-secret: basic-auth
+      nginx.ingress.kubernetes.io/auth-realm: "Enter your credentials"
+  prometheusSpec:
+    enableRemoteWriteReceiver: true
 grafana:
   enabled: false
   forceDeployDashboards: true
