9fe502093cab289cdc263af39f4fb27b8ee0db86
Ulli Goschler <git@nce.wtf>
2025-03-30 00:05:24 +0100

    chore(cleanup): after switching to cloudflare, remove oracle dns management

--- terraform/config/_output.tf
+++ terraform/config/_output.tf
@@ -3,27 +3,3 @@
 #
 #   sensitive = true
 # }
-
-output "homelab_dns_key" {
-  value = module.homelab.dns_privkey
-
-  sensitive = true
-}
-
-output "homelab_dns_tenancy" {
-  value = module.homelab.dns_tenancy
-
-  sensitive = true
-}
-
-output "homelab_dns_user" {
-  value = module.homelab.dns_user
-
-  sensitive = true
-}
-
-output "homelab_dns_api_fingerprint" {
-  value = module.homelab.dns_api_fingerprint
-
-  sensitive = true
-}
