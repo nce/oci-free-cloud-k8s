8c2146f6d3bcee6f23ef2c24b8bf9507b37a1b7d
Ulli Goschler <git@nce.wtf>
2025-04-02 15:59:13 +0200

    feat(oci): update node image automagically

--- terraform/infra/_variables.tf
+++ terraform/infra/_variables.tf
@@ -22,7 +22,7 @@ variable "kubernetes_version" {
   description = "Version of Kubernetes"
   type        = string
 
-  default = "v1.29.1"
+  default = "v1.32.1"
 }
 
 variable "kubernetes_worker_nodes" {
@@ -30,28 +30,4 @@ variable "kubernetes_worker_nodes" {
   type        = number
 
   default = 2
-}
-
-# TODO: search for latest image
-variable "image_id" {
-  # https://docs.oracle.com/en-us/iaas/images/oke-worker-node-oracle-linux-8x/
-  description = "OCID of the latest oracle linux"
-  type        = string
-
-  # Oracle-Linux-8.9-aarch64-2024.01.26-0-OKE-1.29.1-679 // 21.02.2024
-  default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa3mctwlkbkaj76pnlrmdr7w6dd4frkrhuqrdadpq4g67kh5gqbn3q"
-}
-
-variable "klangregen_domain_name" {
-  description = "Main DNS Zone"
-  type        = string
-
-  default = "klangregen.de"
-}
-
-variable "wtf_domain_name" {
-  description = "DNS Zone"
-  type        = string
-
-  default = "nce.wtf"
 }
