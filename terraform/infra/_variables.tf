885c636e8375d8ff9bccd0c705fcc72443704ca3
Ulli Goschler <git@nce.wtf>
2025-08-07 11:41:18 +0200

    Fix DNS (#140)
    * feat(k8s): upgrade to latest
    
    * feat(dns): remove http01 challenge
    
    * feat(dns): switch to unique domain name
    
    * fix(flux): default flux path
    
    * fix readme
    
    * fix
    
    * fix

--- terraform/infra/_variables.tf
+++ terraform/infra/_variables.tf
@@ -22,7 +22,7 @@ variable "kubernetes_version" {
   description = "Version of Kubernetes"
   type        = string
 
-  default = "v1.32.1"
+  default = "v1.33.1"
 }
 
 variable "kubernetes_worker_nodes" {
